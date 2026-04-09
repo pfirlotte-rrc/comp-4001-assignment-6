561fe2bd5afb751564d2d76ba3f058e3c0981683
Peter <pfirlotte@academic.rrc.ca>
2026-04-09 13:17:13 -0500

    Corrected port errors and added delete script..

---
+++ deleteMinikubeSetup.sh
@@ -0,0 +1,15 @@
+kubectl delete deploy backend nginx transactions mongo studentportfolio --ignore-not-found
+
+kubectl delete service backend kubernetes nginx transactions mongo studentportfolio --ignore-not-found
+
+minikube stop
+
+minikube delete
+
+sudo docker rmi $(docker images -aq)
+
+sudo docker system prune -a
+
+minikube delete –all
+
+minikube delete –purge
